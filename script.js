daeb0fad65f3bd3620bc39b72512631b20407665
nakinnubis <abiola.akinnubi@gmail.com>
2017-09-04 11:03:10 +0100

    tweaking the application to redircet to html version

--- script.js
+++ script.js
@@ -30,14 +30,14 @@ function init() {
     var mtlLoader = new THREE.MTLLoader();
     mtlLoader.setBaseUrl('assets/');
     mtlLoader.setPath('assets/');
-    mtlLoader.load('female-croupier-2013-03-26.mtl', function (materials) {
+    mtlLoader.load('shirt.mtl', function (materials) {
         materials.preload();
         materials.materials.default.map.magFilter = THREE.NearestFilter;
         materials.materials.default.map.minFilter = THREE.LinearFilter;
         var objLoader = new THREE.OBJLoader();
         objLoader.setMaterials(materials);
         objLoader.setPath('assets/');
-        objLoader.load('female-croupier-2013-03-26.obj', function (object) {
+        objLoader.load('shirt.obj', function (object) {
             scene.add(object);
         });
     });
